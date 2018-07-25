7ecc3eab855086184b5b0978eb7cc981f59fa143
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-07-25 17:23:51 +0900

    4章のコード類をコミット

---
+++ chap04/12_LessThan/src/main/scala/Subtract4Bit.scala
@@ -0,0 +1,20 @@
+import chisel3._
+
+class Subtract4Bit extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(4.W))
+    val b = Input(UInt(4.W))
+    val result = Output(UInt(4.W))
+  })
+
+  val adder = Module(new Adder4Bit)
+  adder.io.a := io.a
+  adder.io.b := ~(io.b)
+  adder.io.carryIn := 1.U
+
+  io.result := adder.io.sum
+}
+
+object Subtract4Bit extends App {
+  chisel3.Driver.execute(args, () => new Subtract4Bit)
+}
