27ef8be6ff5ad4ba5e09f80ac6c87f7e92227f9c
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-07-28 16:04:51 +0900

    5章のコードをコミット

---
+++ chap05/11_PS2Keyboard/src/main/scala/ShiftRegisterSIPO.scala
@@ -0,0 +1,22 @@
+import chisel3._
+import chisel3.util._
+
+/** シフトレジスタ。直列入力-並列出力形(Serial-In, Parallel-Out)
+  * 
+  * @param n 出力ビット幅
+  */
+class ShiftRegisterSIPO(n: Int) extends Module {
+  val io = IO(new Bundle {
+    val shiftIn = Input(Bool()) // 入力。
+    val enable = Input(Bool())  // trueの時にshiftInを取り込みます。
+    val q = Output(UInt(n.W))   // 出力。古い入力が上位ビットになります。
+  })
+
+  val reg = RegInit(0.U(n.W))
+  when (io.enable) {
+    reg := Cat(reg(n - 2, 0), io.shiftIn)
+  }
+
+  io.q := reg
+}
+
---
+++ chap05/15_SvgaUartDisp/src/main/scala/DisplayClock.scala
@@ -0,0 +1,11 @@
+import chisel3._
+import chisel3.util._
+
+/** DisplayClock
+  */
+class DisplayClock extends BlackBox with HasBlackBoxInline {
+  val io = IO(new Bundle {
+    val clk_system = Input(Clock())
+    val clk_display = Output(Clock())
+  })
+}
